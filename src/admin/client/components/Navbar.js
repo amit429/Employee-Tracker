096aead842ee0b38aece95e6fb49723a4ab16c58
Your Name <you@example.com>
2022-10-09 11:48:25 +0530

    navbar

---
+++ src/admin/client/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import { Avatar, Typography } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <>
+        <AppBar position="static" sx={{
+            padding: '20px',
+            backgroundColor: 'purple',
+            display: 'flex',
+            flexDirection: 'row',
+        }}>
+           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Employee Tracker Dashboard
+           </Typography>
+
+           <Avatar alt="Amit Pile" sx={{
+                marginRight: '20px',
+                backgroundColor: 'orange',
+                padding: '5px',
+           }}>
+                AP
+           </Avatar>
+        </AppBar>
+    </>
+  )
+}
