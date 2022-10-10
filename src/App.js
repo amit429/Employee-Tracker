7f087c0a7be95fd59bb81c7f3c5aa50da10d619c
Amit Pile <1032202075@mitwpu.edu.in>
2022-10-10 14:55:17 +0530

    login frontend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import AdminDashboard from './admin/client/Dashboard';
 import EmployeeDashboard from './employee/client/EmployeeDashboard';
+import Login from './common/Login';
+import Landing from './common/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/admin-login" element={<Login title="Admin Login" />} />
+          <Route path="/employee-login" element={<Login title="Employee Login" />} />
           <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
           <Route path="/employee-dashboard" element={<EmployeeDashboard/>}/>
         </Routes>
