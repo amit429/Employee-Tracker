78ec6f02347b8ff7933db59311908e2b47925a73
Anmol Prasad <anmol.gpc@gmail.com>
2022-10-10 15:16:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import "./App.css";
 import AdminDashboard from "./admin/client/Dashboard";
 import EmployeeDashboard from "./employee/client/EmployeeDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { DataProvider } from "./Globalstate.js";
 
 function App() {
   return (
     <>
-      <DataProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-            <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
